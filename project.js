cdf318388ede4a7e43eb0b26de6fbdd69aca202f
root <root@DESKTOP-TCD1NHF>
2023-03-03 15:55:10 -0600

    Project Progress

--- project.js
+++ project.js
@@ -1,12 +1,24 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f7f6c1d57bmsh585d996ad37a94dp10319bjsna477fed8dbc3',
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-	}
-};
 
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const movieDisplay = document.getElementById("movieDisplay")
+
+
+
+function displayMovies() {
+    fetch("https://swapi.dev/api/films/")
+    .then(response => response.json())
+    .then(movies => {
+        const x = movies.results
+        const movieList = x.map(function(movie) {
+            return `
+                <h1>${movie.title}</h1>
+                <a href="https://m.media-amazon.com/images/I/81P3lDJbjCL.jpg"><img src="https://m.media-amazon.com/images/I/81P3lDJbjCL.jpg" width="200px"></img></a>
+            `
+            
+        })
+        movieDisplay.innerHTML = movieList.join("")
+    })
+
+}
+displayMovies()
+
+
