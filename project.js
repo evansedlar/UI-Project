737b29c671c13970f0217e1114a14606a58c2b06
root <root@DESKTOP-TCD1NHF>
2023-03-06 16:34:57 -0600

    updated project

--- project.js
+++ project.js
@@ -7,51 +7,210 @@ const vehiclesButton = document.getElementById("vehiclesButton")
 const starshipsButton = document.getElementById("starshipsButton")
 const pageContents = document.getElementById('pageContents')
 const imageList = document.getElementById('movieImg')
+const loadingScreen = document.getElementById('loading-screen')
 const images = Array.from(imageList.getElementsByTagName('img'))
 let currentImageIndex = 0
 const numImgToShow = 3
 
-homeButton.addEventListener('click', function() {
+loadingScreen.style.display = "none"
+
+
+//Home Button
+homeButton.addEventListener('click', function () {
     window.location.href = "project.html";
 })
 
-
-planetsButton.addEventListener('click', function() {
+// Planets Button
+planetsButton.addEventListener('click', function () {
     function displayPlanets() {
         movieImg.innerHTML = ""
         pageContents.innerHTML = ""
-        fetch("https://swapi.dev/api/planets/")
-        .then(response => response.json())
-        .then(planetsArr => {
-            const planets = planetsArr.results
-            const planetsList = planets.map(function(planet) {
-                return `
+        loadingScreen.style.display = "block"
+        fetch("https://swapi.dev/api/planets")
+            .then(response => response.json())
+            .then(planetsArr => {
+                const planets = planetsArr.results
+                const planetsList = planets.map(function (planet) {
+                    return `
                 <ul id="planetInfo">
-                    <h1>Planet: ${planet.name}</h1>
-                    <li>Climate: ${planet.climate}</li>
-                    <li>Terrain: ${planet.terrain}</li>
-                    <li>Population: ${planet.population}</li>
+                    <h1>${planet.name}</h1>
+                    <li>Homeworld: ${planet.homeworld}</li>
+                    <li>Language: ${planet.language}</li>
+                    <a href="${planet.people}">Characters of this species</a>
                 </ul>
                 `
+                })
+                pageContents.innerHTML = planetsList.join("")
+
+                loadingScreen.style.display = "none"
             })
-            pageContents.innerHTML = planetsList.join("")
-        })
+            .catch(error => {
+                loadingScreen.style.display = "none"
+                console.error(error)
+            })
     }
     displayPlanets()
 })
 
 
-for(let i = 0; i < images.length; i++) {
-    if (i >= numImgToShow) { 
+
+
+
+
+
+//Species Button
+speciesButton.addEventListener('click', function () {
+    function displaySpecies() {
+        movieImg.innerHTML = ""
+        pageContents.innerHTML = ""
+        loadingScreen.style.display = "block"
+        fetch("https://swapi.dev/api/species")
+            .then(response => response.json())
+            .then(speciesArr => {
+                const species = speciesArr.results
+                const speciesList = species.map(function (specie) {
+                    return `
+                <ul id="planetInfo">
+                    <h1>${specie.name}</h1>
+                    <li>Homeworld: ${specie.homeworld}</li>
+                    <li>Language: ${specie.language}</li>
+                    <a href="${specie.people}">Characters of this species</a>
+                </ul>
+                `
+                })
+                pageContents.innerHTML = speciesList.join("")
+
+                loadingScreen.style.display = "none"
+            })
+            .catch(error => {
+                loadingScreen.style.display = "none"
+                console.error(error)
+            })
+    }
+    displaySpecies()
+})
+
+
+//Characters Button
+charactersButton.addEventListener('click', function () {
+    function displayCharacters() {
+        movieImg.innerHTML = ""
+        pageContents.innerHTML = ""
+        loadingScreen.style.display = "block"
+        fetch("https://swapi.dev/api/people/")
+            .then(response => response.json())
+            .then(charactersArr => {
+                const characters = charactersArr.results
+                const charactersList = characters.map(function (character) {
+                    return `
+                <ul id="planetInfo">
+                    <h1>${character.name}</h1>
+                    <li>Height: ${character.height}cm</li>
+                    <li>Hair Color: ${character.hair_color}</li>
+                    <li>Eye Color: ${character.eye_color}</li>
+                    <li>Birth Year: ${character.birth_year}</li>
+                    <li>Gender: ${character.gender}</li>
+                </ul>
+                `
+                })
+                pageContents.innerHTML = charactersList.join("")
+
+                loadingScreen.style.display = "none"
+            })
+            .catch(error => {
+                loadingScreen.style.display = "none"
+                console.error(error)
+            })
+    }
+    displayCharacters()
+})
+
+
+//Vehicles Button
+vehiclesButton.addEventListener('click', function () {
+    function displayVehicles() {
+        movieImg.innerHTML = ""
+        pageContents.innerHTML = ""
+        loadingScreen.style.display = "block"
+        fetch("https://swapi.dev/api/vehicles/")
+            .then(response => response.json())
+            .then(vehiclesArr => {
+                const vehicles = vehiclesArr.results
+                const vehiclesList = vehicles.map(function (vehicle) {
+                    return `
+                <ul id="planetInfo">
+                    <h1>${vehicle.name}</h1>
+                    <li>Model: ${vehicle.model}</li>
+                    <li>Manufacturer: ${vehicle.manufacturer}</li>
+                    <li>Cost in Credits: ${vehicle.cost_in_credits}</li>
+                    <li>Vehicle Class: ${vehicle.vehicle_class}</li>
+                    <li>Crew Size: ${vehicle.crew}</li>
+                </ul>
+                `
+                })
+                pageContents.innerHTML = vehiclesList.join("")
+
+                loadingScreen.style.display = "none"
+            })
+            .catch(error => {
+                loadingScreen.style.display = "none"
+                console.error(error)
+            })
+    }
+    displayVehicles()
+})
+
+
+//Starships Button
+starshipsButton.addEventListener('click', function () {
+    function displayStarships() {
+        movieImg.innerHTML = ""
+        pageContents.innerHTML = ""
+        loadingScreen.style.display = "block"
+        fetch("https://swapi.dev/api/starships/")
+            .then(response => response.json())
+            .then(starshipsArr => {
+                const starships = starshipsArr.results
+                const starshipsList = starships.map(function (starship) {
+                    return `
+                <ul id="planetInfo">
+                    <h1>${starship.name}</h1>
+                    <li>Model: ${starship.model}</li>
+                    <li>Manufacturer: ${starship.manufacturer}</li>
+                    <li>Cost in Credits: ${starship.cost_in_credits}</li>
+                    <li>Vehicle Class: ${starship.starship_class}</li>
+                    <li>Crew Size: ${starship.crew}</li>
+                </ul>
+                `
+                })
+                pageContents.innerHTML = starshipsList.join("")
+
+                loadingScreen.style.display = "none"
+            })
+            .catch(error => {
+                loadingScreen.style.display = "none"
+                console.error(error)
+            })
+    }
+    displayStarships()
+})
+
+
+
+
+
+
+for (let i = 0; i < images.length; i++) {
+    if (i >= numImgToShow) {
         images[i].style.display = 'none'
     }
 }
 
-showImages()
+
 
 function showImages() {
     for (let i = currentImageIndex; i < currentImageIndex + numImgToShow; i++) {
-        if( i >= images.length) {
+        if (i >= images.length) {
             currentImageIndex = 0;
             i = currentImageIndex;
         }
@@ -67,3 +226,4 @@ function showImages() {
     setTimeout(showImages, 5000);
 }
 
+showImages()
