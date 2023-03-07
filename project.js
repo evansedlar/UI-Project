f3860573aff87969cdc6ea60d1750c50cc0d619d
root <root@DESKTOP-TCD1NHF>
2023-03-07 13:47:48 -0600

    project updates

--- project.js
+++ project.js
@@ -8,6 +8,10 @@ const starshipsButton = document.getElementById("starshipsButton")
 const pageContents = document.getElementById('pageContents')
 const imageList = document.getElementById('movieImg')
 const loadingScreen = document.getElementById('loading-screen')
+//next buttons
+
+
+
 const next = document.getElementById('next')
 const prev = document.getElementById('prev')
 const images = Array.from(imageList.getElementsByTagName('img'))
@@ -22,30 +26,33 @@ homeButton.addEventListener('click', function () {
     window.location.href = "project.html";
 })
 
-// Planets Button
+// Planets Buttons 1-6
 planetsButton.addEventListener('click', function() {
     function displayPlanets() {
         movieImg.innerHTML = ""
         pageContents.innerHTML = ""
         loadingScreen.style.display = "block"
-        fetch("https://swapi.dev/api/planets")
+        fetch("https://swapi.dev/api/planets/")
             .then(response => response.json())
             .then(planetsArr => {
+                const nextPlanets2 = `<button onclick="planetsPage2()">Next Page</button>`
                 const planets = planetsArr.results
-                const planetsList = planets.map(function (planet) {
+                const planetsList = planets.map(function(planet) {
                     return `
-                <ul id="planetInfo">
+                <ul>
                     <h1>${planet.name}</h1>
-                    <li>Homeworld: ${planet.homeworld}</li>
-                    <li>Language: ${planet.language}</li>
-                    <a href="${planet.people}">Characters of this species</a>
+                    <li>Climate: ${planet.climate}</li>
+                    <li>Terrain: ${planet.terrain}</li>
+                    <li>Population: ${planet.population}</li>
                 </ul>
                 `
-                })
-                pageContents.innerHTML = planetsList.join("")
 
+                })
+                pageContents.innerHTML = planetsList.join("") 
+                buttonContainer.innerHTML = nextPlanets2
                 loadingScreen.style.display = "none"
             })
+            
             .catch(error => {
                 loadingScreen.style.display = "none"
                 console.error(error)
@@ -54,13 +61,132 @@ planetsButton.addEventListener('click', function() {
     displayPlanets()
 })
 
+function planetsPage2() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/planets/?page=2")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextPlanets3 = `<button onclick="planetsPage3()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextPlanets3
+        loadingScreen.style.display = "none"
+    })
+}
+
+function planetsPage3() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/planets/?page=3")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextPlanets4 = `<button onclick="planetsPage4()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextPlanets4
+        loadingScreen.style.display = "none"
+    })
+}
+
+function planetsPage4() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/planets/?page=4")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextPlanets5 = `<button onclick="planetsPage5()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextPlanets5
+        loadingScreen.style.display = "none"
+    })
+}
+
+function planetsPage5() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/planets/?page=5")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextPlanets6 = `<button onclick="planetsPage6()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextPlanets6
+        loadingScreen.style.display = "none"
+    })
+}
+
+function planetsPage6() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/planets/?page=6")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        loadingScreen.style.display = "none"
+    })
+}
 
 
 
-
-
-
-//Species Button
+//Species Buttons 1-4
 speciesButton.addEventListener('click', function () {
     function displaySpecies() {
         movieImg.innerHTML = ""
@@ -69,6 +195,7 @@ speciesButton.addEventListener('click', function () {
         fetch("https://swapi.dev/api/species")
             .then(response => response.json())
             .then(speciesArr => {
+                const nextSpecies2 = `<button onclick="speciesPage2()">Next Page</button>`
                 const species = speciesArr.results
                 const speciesList = species.map(function (specie) {
                     return `
@@ -81,7 +208,7 @@ speciesButton.addEventListener('click', function () {
                 `
                 })
                 pageContents.innerHTML = speciesList.join("")
-
+                buttonContainer.innerHTML =  nextSpecies2
                 loadingScreen.style.display = "none"
             })
             .catch(error => {
@@ -92,6 +219,78 @@ speciesButton.addEventListener('click', function () {
     displaySpecies()
 })
 
+function speciesPage2() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/species?page=2")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextSpecies3 = `<button onclick="speciesPage3()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextSpecies3
+        loadingScreen.style.display = "none"
+    })
+}
+
+function speciesPage3() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/species?page=3")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const nextSpecies4 = `<button onclick="speciesPage4()">Next Page</button>`
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        buttonContainer.innerHTML = nextSpecies4
+        loadingScreen.style.display = "none"
+    })
+}
+
+function speciesPage4() {
+    pageContents.innerHTML = ""
+    buttonContainer.innerHTML = ""
+    loadingScreen.style.display = "block"
+    fetch("https://swapi.dev/api/species?page=4")
+    .then(response => response.json())
+    .then(planetsArr => {
+        const planets = planetsArr.results
+        const planetsList = planets.map(function (planet) {
+            return `
+        <ul>
+            <h1>${planet.name}</h1>
+            <li>Climate: ${planet.climate}</li>
+            <li>Terrain: ${planet.terrain}</li>
+            <li>Population: ${planet.population}</li>
+        </ul>
+            `
+        })
+        pageContents.innerHTML = planetsList.join("")
+        loadingScreen.style.display = "none"
+    })
+}
 
 //Characters Button
 charactersButton.addEventListener('click', function () {
@@ -196,8 +395,6 @@ starshipsButton.addEventListener('click', function () {
     }
     displayStarships()
 })
-
-
 
 
 
